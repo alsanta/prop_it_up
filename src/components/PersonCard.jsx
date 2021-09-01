cc0df95a49966c72fe2b77190973bc5b5c3ac330
Alex Santana <alexandresntn@yahoo.com>
2021-09-01 16:25:57 -0500

    Added state usage

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 
 class PersonCard extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            age: this.props.age
+        };
+    }
+
     render(){
+        const addAge = ()=>{
+            this.setState({age: this.state.age+1})
+        }
         return (
             <>
             <h1>{this.props.name}</h1>
-            <p>Age: {this.props.age}</p>
+            <p>Age: {this.state.age}</p>
             <p>Hair color: {this.props.hair}</p>
+            <button onClick={addAge} >Happy Birthday!</button>
             </>
         );
     }
